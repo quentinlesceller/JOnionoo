c9a784759ccc04bf998e8585fd265e3134814cda
Quentin Le Sceller <q.lesceller@gmail.com>
2015-12-18 11:38:52 -0500

    changed names

---
+++ src/main/java/jonionoo/JOnionoo.java
@@ -0,0 +1,45 @@
+package jonionoo;
+
+import java.util.ArrayList;
+
+import objects.Relay;
+import parser.IParser;
+import parser.OnionooParser;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Jonionoo. Main instance of the program.
+ * 
+ * @author quentin
+ */
+public class JOnionoo {
+
+	/** The parser. */
+	private IParser parser;
+
+	/** The server. */
+	private String server;
+
+	/**
+	 * Instantiates a new atlas.
+	 *
+	 * @param server
+	 *            the server
+	 */
+	public JOnionoo(String server) {
+		this.server = server;
+		parser = new OnionooParser();
+	}
+
+	/**
+	 * Gets the relay from a Onionoo query.
+	 *
+	 * @param query
+	 *            the fingerprint
+	 * @return the relay information
+	 */
+	public ArrayList<Relay> getRelays(String query) {
+
+		return parser.get(server, query);
+	}
+}
